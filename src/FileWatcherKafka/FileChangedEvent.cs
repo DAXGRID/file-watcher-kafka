80cecb6ad0b4b57d3def4c90f59486e4b80d14a5
Rune Nielsen <runenielsen@runbox.com>
2021-10-15 16:53:46 +0200

    adds event id

--- src/FileWatcherKafka/FileChangedEvent.cs
+++ src/FileWatcherKafka/FileChangedEvent.cs
@@ -4,12 +4,14 @@ namespace FileWatcherKafka
 {
     public record FileChangedEvent
     {
+        public Guid EventId { get; init; }
         public string EventType { get; init; }
         public DateTime EventTimeStamp { get; init; }
         public string FullPath { get; init; }
 
         public FileChangedEvent(string fullPath)
         {
+            EventId = Guid.NewGuid();
             EventTimeStamp = DateTime.UtcNow;
             EventType = nameof(FileChangedEvent);
             FullPath = fullPath;
